0cba90873114eff05715cb1a329cc276f2e16af9
Dr.jacky <dr_jacky_2005@yahoo.com>
2017-12-01 09:48:41 +0330

    Object Models added.

---
+++ app/src/main/java/ir/hosseinabbasi/mediamonks/data/db/model/Album.java
@@ -0,0 +1,51 @@
+package ir.hosseinabbasi.mediamonks.data.db.model;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("com.robohorse.robopojogenerator")
+public class Album{
+
+	@SerializedName("id")
+	private int id;
+
+	@SerializedName("title")
+	private String title;
+
+	@SerializedName("userId")
+	private int userId;
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setTitle(String title){
+		this.title = title;
+	}
+
+	public String getTitle(){
+		return title;
+	}
+
+	public void setUserId(int userId){
+		this.userId = userId;
+	}
+
+	public int getUserId(){
+		return userId;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Album{" + 
+			"id = '" + id + '\'' + 
+			",title = '" + title + '\'' + 
+			",userId = '" + userId + '\'' + 
+			"}";
+		}
+}
